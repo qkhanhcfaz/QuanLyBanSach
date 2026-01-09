9613ff2c1eb2ed61e3f3576767a1761316679c34
Tran Van Quoc Khanh <tranvanquockhanh123@gmail.com>
2026-01-09 08:03:52 +0700

    Chuẩn bị khung backend theo mô hình MVC

--- src/routes/postRouter.js
+++
@@ -1,8 +0,0 @@
-// File: /src/routes/postRouter.js
-const express = require('express');
-const { getPublicPosts } = require('../controllers/postController');
-const router = express.Router();
-
-router.route('/public').get(getPublicPosts);
-
-module.exports = router;
