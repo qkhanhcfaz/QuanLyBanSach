f49621272b244ec203759ab36cc2e031eaac928d
Luong Anh Duy <luonganhduy1002@gmail.com>
2026-01-15 10:44:48 +0700

    Làm sạch code

--- src/models/index.js
+++ src/models/index.js
@@ -17,42 +17,6 @@ fs.readdirSync(__dirname)
     );
   })
   .forEach((file) => {
-    // Import model từ file
-    // QUAN TRỌNG: Các file model cũ (productModel.js...) không được viết theo kiểu
-    // module.exports = (sequelize, DataTypes) => ... mà export trực tiếp instance hoặc function.
-    // ĐỂ TƯƠNG THÍCH: Chúng ta cần kiểm tra xem model export ra cái gì.
-
-    // Tuy nhiên, để sửa nhanh theo cách của book_store, ta giả định các model User, Role mới
-    // tuân thủ chuẩn book_store. Còn các model cũ như Product, Category có thể gây lỗi với code dynamic này
-    // nếu chúng không theo chuẩn define(sequelize, DataTypes).
-
-    // KIỂM TRA LẠI: Trong QuanLyBanSach, index.js cũ import kiểu:
-    // const Product = require('./productModel');
-    // const Category = require('./categoryModel')(sequelize);
-
-    // productModel.js export trực tiếp `sequelize.define(...)` -> OK (instance)
-    // categoryModel.js export `module.exports = (sequelize) => sequelize.define(...)` -> Function
-
-    // Code dynamic của book_store (dòng 23: require(path.join(__dirname, file))) sẽ lấy module.exports
-    // Sau đó gán db[model.name] = model.
-    // Nếu model là function (Category), model.name sẽ là tên hàm (hoặc undefined).
-    // Nếu model là instance (Product), model.name là 'Product'.
-
-    // => Code dynamic này CHỈ CHẠY ĐÚNG nếu TẤT CẢ model đều export instance hoặc đều export function define.
-    // `book_store` dùng: const model = require(...)
-    // `userModel.js` của book_store export instance: `module.exports = User;` (dòng 148 step 150)
-
-    // VẬY LÀM SAO ĐỂ HÒA HỢP?
-    // Cách tốt nhất là sửa `index.js` để KHÔNG dùng dynamic loading quá cứng nhắc,
-    // hoặc sửa các model cũ cho chuẩn.
-    // NHƯNG user muốn "giống code dự án book_store".
-
-    // Code book_store index.js đơn giản là require file.
-    // Nếu file export instance (User, Product), thì `model` là instance. `model.name` ok.
-    // Nếu file export function (Category), `model` là function. `model.name` là tên hàm.
-    // Ta cần gọi function đó để lấy instance.
-
-    // Sửa đổi nhẹ logic dynamic loading để hỗ trợ cả 2 kiểu:
     const modelDef = require(path.join(__dirname, file));
     let model;
     if (
