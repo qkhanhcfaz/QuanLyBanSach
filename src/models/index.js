d00204f6467a083e9fad5c6518929de5298612c8
Luong Anh Duy <luonganhduy1002@gmail.com>
2026-01-15 08:51:19 +0700

    Sửa quá trời lỗi

--- src/models/index.js
+++ src/models/index.js
@@ -1,32 +1,88 @@
-// File: /src/models/index.js
-// File tổng hợp tất cả models và thiết lập quan hệ (associations)
+"use strict";
 
-const { sequelize } = require('../config/connectDB');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const { sequelize } = require("../config/connectDB");
+const basename = path.basename(__filename);
 
-// Import các models - productModel đã export trực tiếp model instance
-const Product = require('./productModel');
-const Category = require('./categoryModel')(sequelize);
-const Slideshow = require('./slideshowModel')(sequelize);
-const Post = require('./postModel')(sequelize);
+const db = {};
 
-// === THIẾT LẬP QUAN HỆ GIỮA CÁC MODELS ===
-// Product thuộc về Category
-Product.belongsTo(Category, {
-    foreignKey: 'danh_muc_id',
-    as: 'category'
+// Đọc tất cả các file trong thư mục hiện tại
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    // Lọc ra các file javascript, không phải file ẩn, không phải file index.js
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
+  })
+  .forEach((file) => {
+    // Import model từ file
+    // QUAN TRỌNG: Các file model cũ (productModel.js...) không được viết theo kiểu
+    // module.exports = (sequelize, DataTypes) => ... mà export trực tiếp instance hoặc function.
+    // ĐỂ TƯƠNG THÍCH: Chúng ta cần kiểm tra xem model export ra cái gì.
+
+    // Tuy nhiên, để sửa nhanh theo cách của book_store, ta giả định các model User, Role mới
+    // tuân thủ chuẩn book_store. Còn các model cũ như Product, Category có thể gây lỗi với code dynamic này
+    // nếu chúng không theo chuẩn define(sequelize, DataTypes).
+
+    // KIỂM TRA LẠI: Trong QuanLyBanSach, index.js cũ import kiểu:
+    // const Product = require('./productModel');
+    // const Category = require('./categoryModel')(sequelize);
+
+    // productModel.js export trực tiếp `sequelize.define(...)` -> OK (instance)
+    // categoryModel.js export `module.exports = (sequelize) => sequelize.define(...)` -> Function
+
+    // Code dynamic của book_store (dòng 23: require(path.join(__dirname, file))) sẽ lấy module.exports
+    // Sau đó gán db[model.name] = model.
+    // Nếu model là function (Category), model.name sẽ là tên hàm (hoặc undefined).
+    // Nếu model là instance (Product), model.name là 'Product'.
+
+    // => Code dynamic này CHỈ CHẠY ĐÚNG nếu TẤT CẢ model đều export instance hoặc đều export function define.
+    // `book_store` dùng: const model = require(...)
+    // `userModel.js` của book_store export instance: `module.exports = User;` (dòng 148 step 150)
+
+    // VẬY LÀM SAO ĐỂ HÒA HỢP?
+    // Cách tốt nhất là sửa `index.js` để KHÔNG dùng dynamic loading quá cứng nhắc,
+    // hoặc sửa các model cũ cho chuẩn.
+    // NHƯNG user muốn "giống code dự án book_store".
+
+    // Code book_store index.js đơn giản là require file.
+    // Nếu file export instance (User, Product), thì `model` là instance. `model.name` ok.
+    // Nếu file export function (Category), `model` là function. `model.name` là tên hàm.
+    // Ta cần gọi function đó để lấy instance.
+
+    // Sửa đổi nhẹ logic dynamic loading để hỗ trợ cả 2 kiểu:
+    const modelDef = require(path.join(__dirname, file));
+    let model;
+    if (
+      typeof modelDef === "function" &&
+      !modelDef.tableName &&
+      !modelDef.name
+    ) {
+      // Trường hợp export function(sequelize) => return model matches Category, Post...
+      // Nhưng chờ đã, CategoryModel.js cũ cần tham số sequelize.
+      model = modelDef(sequelize, Sequelize.DataTypes);
+    } else {
+      // Trường hợp export instance (User, Role, Product)
+      model = modelDef;
+    }
+
+    // Gán vào db
+    if (model && model.name) {
+      db[model.name] = model;
+    }
+  });
+
+// Sau khi đã import TẤT CẢ các model, lúc này mới gọi hàm associate
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
-// Category có nhiều Products
-Category.hasMany(Product, {
-    foreignKey: 'danh_muc_id',
-    as: 'products'
-});
+// Gắn sequelize instance và class Sequelize vào đối tượng db
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-// === EXPORT CÁC MODELS VÀ SEQUELIZE INSTANCE ===
-module.exports = {
-    sequelize,
-    Category,
-    Product,
-    Slideshow,
-    Post
-};
+module.exports = db;
--- src/models/productModel.js
+++ src/models/productModel.js
@@ -1,126 +1,138 @@
 // File: /src/models/productModel.js
-const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/connectDB');
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/connectDB");
 
 // Định nghĩa model 'Product' tương ứng với bảng 'products' trong CSDL
-const Product = sequelize.define('Product', {
+const Product = sequelize.define(
+  "Product",
+  {
     // Sequelize sẽ tự động tạo cột 'id' làm khóa chính (primary key)
-    
+
     ten_sach: {
-        type: DataTypes.STRING, 
-        allowNull: false 
+      type: DataTypes.STRING,
+      allowNull: false,
     },
 
     mo_ta_ngan: {
-        type: DataTypes.TEXT, 
-        allowNull: true 
+      type: DataTypes.TEXT,
+      allowNull: true,
     },
 
     gia_bia: {
-        type: DataTypes.DECIMAL(12, 2), 
-        allowNull: false
+      type: DataTypes.DECIMAL(12, 2),
+      allowNull: false,
     },
 
     so_luong_ton_kho: {
-        type: DataTypes.INTEGER, 
-        allowNull: false,
-        defaultValue: 0 
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
     },
 
     // --- [MỚI] THÊM CỘT NÀY ĐỂ TÍNH BEST SELLER ---
     da_ban: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 0, // Mặc định là 0 khi tạo mới
-        comment: 'Số lượng sản phẩm đã bán (Dùng để sắp xếp Top Bán Chạy)'
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0, // Mặc định là 0 khi tạo mới
+      comment: "Số lượng sản phẩm đã bán (Dùng để sắp xếp Top Bán Chạy)",
     },
     // ----------------------------------------------
 
     tac_gia: {
-        type: DataTypes.STRING,
-        allowNull: true
+      type: DataTypes.STRING,
+      allowNull: true,
     },
 
     nha_xuat_ban: {
-        type: DataTypes.STRING,
-        allowNull: true
+      type: DataTypes.STRING,
+      allowNull: true,
     },
 
     nam_xuat_ban: {
-        type: DataTypes.INTEGER,
-        allowNull: true
+      type: DataTypes.INTEGER,
+      allowNull: true,
     },
 
     img: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        comment: 'URL đến hình ảnh đại diện của sản phẩm'
+      type: DataTypes.STRING,
+      allowNull: true,
+      comment: "URL đến hình ảnh đại diện của sản phẩm",
     },
 
     so_trang: {
-        type: DataTypes.INTEGER, 
-        allowNull: true,
-        validate: {
-            isInt: true,      
-            min: 1            
-        }
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        isInt: true,
+        min: 1,
+      },
     },
 
     product_type: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        defaultValue: 'print_book', 
-        comment: 'Loại sản phẩm: sách in (print_book) hoặc ebook'
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "print_book",
+      comment: "Loại sản phẩm: sách in (print_book) hoặc ebook",
     },
 
     ebook_url: {
-        type: DataTypes.STRING,
-        allowNull: true, 
-        comment: 'Đường dẫn gốc đến file ebook'
+      type: DataTypes.STRING,
+      allowNull: true,
+      comment: "Đường dẫn gốc đến file ebook",
     },
 
     danh_muc_id: {
-        type: DataTypes.BIGINT,
-        allowNull: false
-    }
-
-}, {
-    tableName: 'products', 
-    timestamps: true, 
-});
+      type: DataTypes.BIGINT,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "products",
+    timestamps: true,
+  },
+);
 
 // Định nghĩa các mối quan hệ (associations)
 Product.associate = (models) => {
-
+  if (models.Category) {
     Product.belongsTo(models.Category, {
-        foreignKey: 'danh_muc_id',
-        as: 'category' 
+      foreignKey: "danh_muc_id",
+      as: "category",
     });
+  }
 
+  if (models.OrderItem) {
     Product.hasMany(models.OrderItem, {
-        foreignKey: 'product_id',
-        as: 'orderItems',
-        onDelete: 'CASCADE', 
-        hooks: true 
+      foreignKey: "product_id",
+      as: "orderItems",
+      onDelete: "CASCADE",
+      hooks: true,
     });
+  }
 
+  if (models.CartItem) {
     Product.hasMany(models.CartItem, {
-         foreignKey: 'product_id', 
-         onDelete: 'CASCADE', 
-         hooks: true 
+      foreignKey: "product_id",
+      onDelete: "CASCADE",
+      hooks: true,
     });
+  }
 
+  if (models.Review) {
     Product.hasMany(models.Review, {
-        foreignKey: 'product_id',
-        as: 'reviews',
-        onDelete: 'CASCADE', 
-        hooks: true 
+      foreignKey: "product_id",
+      as: "reviews",
+      onDelete: "CASCADE",
+      hooks: true,
     });
-    
+  }
+
+  if (models.ReceiptItem) {
     Product.hasMany(models.ReceiptItem, {
-        foreignKey: 'product_id',
-        as: 'receiptHistory' 
+      foreignKey: "product_id",
+      as: "receiptHistory",
     });
+  }
 };
 
 module.exports = Product;
