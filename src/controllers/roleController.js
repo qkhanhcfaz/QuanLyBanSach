258ee9f311ec8791da6d78d8e76c4e8b5eb5ddfd
huathanhnhat <0306221048@gmail.com>
2026-01-18 22:52:27 +0700

    roleController.js: api lấy danh sách vai trò

---
+++ src/controllers/roleController.js
@@ -0,0 +1,16 @@
+const db = require('../models');
+const { Role } = db;
+
+const getAllRoles = async (req, res) => {
+    try {
+        const roles = await Role.findAll({ attributes: ['id', 'ten_quyen'] });
+        res.status(200).json(roles);
+    } catch (error) {
+        console.error('Error fetching roles:', error);
+        res.status(500).json({ message: 'Lỗi server' });
+    }
+};
+
+module.exports = {
+    getAllRoles
+};
